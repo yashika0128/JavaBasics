d7c70a7e7359d9b45da1be0c230afd70bdc2203a
yashika0128 <chaubeyyashika3@gmail.com>
2025-09-16 23:40:01 +0530

    pattern08

---
+++ src/Lecture04/pattern08.java
@@ -0,0 +1,18 @@
+package Lecture04;
+import java.util.*;
+public class pattern08 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int n= sc.nextInt();
+        int num =1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+
+            System.out.println();
+        }
+    }
+}
