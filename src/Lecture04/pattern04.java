d6d4dfd1a4c07e1288670b48312336d0361cee07
yashika0128 <chaubeyyashika3@gmail.com>
2025-09-16 22:29:56 +0530

    pattern04

---
+++ src/Lecture04/pattern04.java
@@ -0,0 +1,16 @@
+package Lecture04;
+import java.util.*;
+public class pattern04 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int row= sc.nextInt();
+
+        for(int i=0 ;i<row; i++){
+            for(int j=row ; j>i; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
