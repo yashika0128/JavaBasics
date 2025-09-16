9e11cc6af4ce6788d57d91eab69937824b2a7d7f
yashika0128 <chaubeyyashika3@gmail.com>
2025-09-16 22:47:41 +0530

    pattern05

---
+++ src/Lecture04/pattern05.java
@@ -0,0 +1,19 @@
+package Lecture04;
+import java.util.*;
+public class pattern05 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            //for spaces
+            for(int j= n-1; j>i; j--) {
+                System.out.print(" ");
+            }
+            //for stars
+            for(int j=0; j<i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
