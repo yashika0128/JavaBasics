02e4a402b9f901ce10c537ee3ad40b645b7666f9
yashika0128 <chaubeyyashika3@gmail.com>
2025-09-17 21:29:13 +0530

    pattern 12 advance pattern

---
+++ src/Lecture05/pattern12.java
@@ -0,0 +1,21 @@
+package Lecture05;
+import java.util.*;
+public class pattern12 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int n= sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            //spaces
+            for(int j=n-(i+1); j>=0; j--){
+                System.out.print(" ");
+            }
+            //numbers
+            for(int j=0; j<i+1; j++){
+                System.out.print(i+1 +" ");
+            }
+            System.out.println();
+        }
+    }
+}
