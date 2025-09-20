1676522446ab7c1584c886d710a30cce778fb1bb
yashika0128 <chaubeyyashika3@gmail.com>
2025-09-20 15:18:39 +0530

    Function to print name(Basic example)

---
+++ src/Lecture06/printName.java
@@ -0,0 +1,17 @@
+package Lecture06;
+
+import java.util.Scanner;
+
+public class printName {
+    public static void printMyName(String name){
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        String me= sc.next();
+        printMyName(me);
+    }
+
+}
