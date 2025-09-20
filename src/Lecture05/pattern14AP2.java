864ed793ef55d3f6f2168f98c39fe08190826d83
yashika0128 <chaubeyyashika3@gmail.com>
2025-09-20 14:08:12 +0530

    pattern 14(AP) 2nd approach

---
+++ src/Lecture05/pattern14AP2.java
@@ -0,0 +1,33 @@
+package Lecture05;
+import java.util.*;
+public class pattern14AP2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+
+        int n= sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            //space -> n- (i+1)
+            for(int j= n-(i+1); j>0; j--){
+                System.out.print(" ");
+            }
+            // stars -> 2*i+1
+            for(int j=0; j<2*i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n; i>0; i--){
+            //space -> n-i
+            for(int j= n-i; j>0; j--){
+                System.out.print(" ");
+            }
+            // stars -> 2*i-1
+            for(int j=0; j<2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
