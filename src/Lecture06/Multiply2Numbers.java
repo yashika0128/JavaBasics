76f4dec35cf1ca033e979c0ca1504706665bb1b9
yashika0128 <chaubeyyashika3@gmail.com>
2025-09-20 15:39:15 +0530

    Function to return the multiplication of two integers(Basic example)

---
+++ src/Lecture06/Multiply2Numbers.java
@@ -0,0 +1,14 @@
+package Lecture06;
+import java.util.*;
+public class Multiply2Numbers {
+    public static int MultiplyNums(int a,int b){
+        return a*b ;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int f= sc.nextInt();
+        int g= sc.nextInt();
+        System.out.println("Multiplication of two numbers is: "+ MultiplyNums(f,g));
+    }
+}
