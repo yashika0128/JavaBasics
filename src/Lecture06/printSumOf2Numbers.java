1d461b56e0f984c5b80844a7c59a1ad168be5a78
yashika0128 <chaubeyyashika3@gmail.com>
2025-09-20 15:30:19 +0530

    Function to return the sum of two integers(Basic example)

---
+++ src/Lecture06/printSumOf2Numbers.java
@@ -0,0 +1,14 @@
+package Lecture06;
+import java.util.*;
+public class printSumOf2Numbers {
+    public static int SumOfNum(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int c= sc.nextInt();
+        int d= sc.nextInt();
+
+        System.out.println(SumOfNum(c,d));
+    }
+}
