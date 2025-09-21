ca89b2163db3d1a7da9ab830626e7c5ada0827ce
yashika0128 <chaubeyyashika3@gmail.com>
2025-09-21 09:25:56 +0530

    Radius and circumference question.modify

--- src/Exercise01/RadiusAndCircumferenceOfCircle.java
+++ src/Exercise01/RadiusAndCircumferenceOfCircle.java
@@ -3,15 +3,14 @@ import java.util.*;
 public class RadiusAndCircumferenceOfCircle {
 
     public static double CircumferencePrint(float r){
-        return 2* Math.PI * r;
+        return 2* Math.PI * r;  // pi is an irrational number so rounding it off will not give the exact correct answer. That's why we use "Math.PI" , it hold the value of PI.
     }
 
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        float radius= sc.nextFloat();
+        float radius = sc.nextFloat();
 
-        System.out.println("Circumference of Circle with radius "+ radius+ " is: "+ CircumferencePrint(radius));
+        System.out.println("Circumference of Circle with radius " + radius + " is: " + CircumferencePrint(radius));
     }
-
 }
